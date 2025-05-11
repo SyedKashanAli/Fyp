11097a32686dd3865e7b63b50b0e5b06974d0bc1
SyedKashanAli-210660 <210660@students.au.edu.pk>
2025-05-11 14:59:38 +0500

    igmore

--- pages/api/deliverypartners/area/[area].js
+++ pages/api/deliverypartners/area/[area].js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 import { NextResponse } from "next/server"
-import { connectionStr } from "../../../lib/db";
-import { deliveryPartnersSchema } from "../../../lib/deliverypartnersModel";
+import dbConnect from "@/middleware/mongoose";
+import DeliveryPartner from "@/models/DeliveryPartner";
 
 export async function GET(request,content){
     let area=content.params.area
     let success=false;
-    await mongoose.connect(connectionStr);
+    await mongoose.connect(dbConnect);
     let filter={area:{$regex:new RegExp(area,'i')}}
-    const result = await deliveryPartnersSchema.find(filter)
+    const result = await DeliveryPartner.find(filter)
     return NextResponse.json({result})
 
 }
